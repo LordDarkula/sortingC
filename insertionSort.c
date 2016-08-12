237a29fdbf6822965c933ec80f7dccb4c9b9160b
Aubhro <aubhrosengupta@gmail.com>
2016-08-12 11:29:08 -0700

    Initial Commit

---
+++ insertionSort.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+
+	int unsorted [6] = { 10, 4, 5, 2, 20, 6 };
+	
+	for(int i = 1; i < sizeof(unsorted)/sizeof(int); i++) {
+		while (unsorted[i] < unsorted[i - 1]) {
+			int temp = unsorted[i];
+			unsorted[i] = unsorted[i - 1];
+			unsorted[i - 1] = temp;
+			
+			if (i == 1) {
+				break;
+			}
+			
+			i--;
+		}
+	}
+	
+	printf("{ ");
+	for(int p = 0; p < sizeof(unsorted)/sizeof(int); p++) {
+	
+		printf("%d ", unsorted[p] );
+	
+	}
+	printf("} \n");
+}
